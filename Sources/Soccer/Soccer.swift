0ea3b3e5f358e820292066c73ddcbebd22d2205c
Jacob Pritchett <jacobpritchett@me.com>
2022-11-19 18:19:51 -0700

    It's a good idea to actually complete your code before publishing it.

--- Sources/Soccer/Soccer.swift
+++ Sources/Soccer/Soccer.swift
@@ -1,15 +1,23 @@
 import Foundation
 
-public protocol SoccerPropertyStoring {
+public protocol SoccerPropertyStoring: NSObject {
     associatedtype T
+    
     func getAssociatedObject(_ key: UnsafeRawPointer!, defaultValue: T?) -> T?
+    func setAssociatedObject(_ key: UnsafeRawPointer!, newValue: T?)
 }
 
 public extension SoccerPropertyStoring {
+    // No stored property support in extensions? Pah, simpletons. Associated Objects to the rescue!
     func getAssociatedObject(_ key: UnsafeRawPointer!, defaultValue: T? = nil) -> T? {
         guard let value = objc_getAssociatedObject(self, key) as? T else {
             return defaultValue
         }
         return value
+    }
+    
+    // All hail the Obj-C runtime!
+    func setAssociatedObject(_ key: UnsafeRawPointer!, newValue: T?) {
+        return objc_setAssociatedObject(self, key, newValue, .OBJC_ASSOCIATION_ASSIGN)
     }
 }
